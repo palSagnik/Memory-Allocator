85e1e6ef6119ab66ead9dd4780f33e04b340a1b1
SAGNIK PAL <sagnik@SAGNIKs-MacBook-Air.local>
2024-02-25 20:02:05 +0530

    First Commit

---
+++ alloc.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <stdio.h>
+#define HEAP_CAPACITY 64000
+
+// our heap
+char heap[HEAP_CAPACITY] = {0};
+size_t heapSize = 0;
+
+void *heapAllocate (size_t size)
+{
+    assert(heapSize + size <= HEAP_CAPACITY);
+
+    // starting of a free heap block
+    void *result = heap + heapSize;
+    heapSize += size;
+    return result;
+
+}
+
+void heapFree (void *ptr)
+{
+    return NULL;
+}
+
+// slide over the heap with a window of 8 bytes
+// and look for allocated memory resembling a pointer to a heap block
+// if we find a pointer that is unreachable, a pointer is reachable if it is pointing to another allocated block of memory on the heap
+// this hence becomes are automatic garbage collector
+void heapCollect ()
+{
+    // this should scan for pointer to the stack as well
+    
+}
