94c3f969e4ed03387605db7f0d2757eaf0009127
SAGNIK PAL <palsagnik2102@gmail.com>
2024-02-29 02:13:58 +0530

    implementation of Separate Data Struc

--- alloc.c
+++ alloc.c
@@ -6,66 +6,100 @@
 #define HEAP_CAPACITY 64000
 #define CHUNK_LIST_CAP 1024
 
-#define TODO \
-    do {\
-        fprintf(stderr , "%s:%d %s is not implemented yet\n", \
-                __FILE__, __LINE__, __func__); \
-        abort(); \
-    } while(0);
+#define TODO                                                 \
+    do                                                       \
+    {                                                        \
+        fprintf(stderr, "%s:%d %s is not implemented yet\n", \
+                __FILE__, __LINE__, __func__);               \
+        abort();                                             \
+    } while (0);
 
-//keep track of the size of the heap chunk
-typedef struct 
+// keep track of the size of the heap chunk
+typedef struct
 {
-    void *start;
+    char *start;
     size_t size;
 
-}Chunk;
+} Chunk;
 
-//using a separate data structure for our metadata
-//this will be a sorted array
-//and we will be adding the new chunks at the end of memory
-typedef struct 
+// using a separate data structure for our metadata
+// this will be a sorted array
+// and we will be adding the new chunks at the end of memory
+typedef struct
 {
     size_t count;
     Chunk chunks[CHUNK_LIST_CAP];
-}ChunkList;
+} ChunkList;
 
-//defining functions for our new ds
-//dump
+// defining functions for our new ds
+// dump
 
-void chunkListDump (const ChunkList *list)
+void chunkListDump(const ChunkList *list)
 {
     printf("Allocated Chunks: %zu\n", list->count);
-    //iterating through the metadata table of allocated chunks and dumping them
-    for(size_t i = 0; i < list->count; i++)
+    // iterating through the metadata table of allocated chunks and dumping them
+    for (size_t i = 0; i < list->count; i++)
     {
-        printf(" Start: %p, Size: %zu\n", list->chunks[i].start, list->chunks[i].size);
+        printf(" Start: %s, Size: %zu\n", list->chunks[i].start, list->chunks[i].size);
     }
 }
-//find
-int chunkListFind (const ChunkList *list, void *ptr)
+
+// compare function for bsearch()
+// weird compar function semantics
+int chunkStartCompar(const void *a, const void *b)
 {
-    //binary search
-    //using bsearch()
-    
+    const Chunk *aChunk = a;
+    const Chunk *bChunk = b;
+
+    return aChunk->start - bChunk->start;
 }
 
-//remove
+// find
+int chunkListFind(const ChunkList *list, void *ptr)
+{
+    // binary search
+    // using bsearch()
+    Chunk key = {
+        .start = ptr};
+
+    Chunk *result = bsearch(&key, list->chunks,
+                            list->count, sizeof(list->chunks[0]),
+                            chunkStartCompar);
+
+    if (result != 0)
+    {
+        // base pointer < chunk pointer
+        assert(list->chunks <= result);
+        return (result - list->chunks) / sizeof(list->chunks[0]);
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+// remove
+// O(list->count)
 void chunkListRemove(ChunkList *list, size_t index)
 {
-    TODO;
+    assert(index < list->count);
+    for (size_t i = index; i < list->count - 1; i++)
+        list->chunks[i] = list->chunks[i + 1];
+
+    list->count--;
 }
 
-//insert
-void chunkListInsert (ChunkList *list, void *start, size_t size)
+// insert
+//O(list->count)
+void chunkListInsert(ChunkList *list, void *start, size_t size)
 {
     assert(list->count < CHUNK_LIST_CAP);
     list->chunks[list->count].start = start;
     list->chunks[list->count].size = size;
 
-    //sorting our chunks using bubble sort
+    // sorting our chunks using bubble sort
     size_t i = list->count;
-    while(list->chunks[i].start < list->chunks[i - 1].start && i != 0)
+    while (list->chunks[i].start < list->chunks[i - 1].start && i != 0)
     {
         const Chunk t = list->chunks[i];
         list->chunks[i] = list->chunks[i - 1];
@@ -81,62 +115,64 @@ void chunkListInsert (ChunkList *list, void *start, size_t size)
 char heap[HEAP_CAPACITY] = {0};
 size_t heapSize = 0;
 
-//metadata for our allocated heap chunks
-//to keep track of the allocated memory
-//the table is used to keep track of the start of each chunk and their sizes
+// metadata for our allocated heap chunks
+// to keep track of the allocated memory
+// the table is used to keep track of the start of each chunk and their sizes
 ChunkList allocatedChunks = {0};
 ChunkList freedChunks = {0};
 
-void *heapAllocate (size_t size)
+void *heapAllocate(size_t size)
 {
-    //taking inspo from malloc that when size is 0,
-    //it either returns a NULL value or a unique pointer which,
-    //can be passed to free
+    // taking inspo from malloc that when size is 0,
+    // it either returns a NULL value or a unique pointer which,
+    // can be passed to free
     if (size == 0)
         return NULL;
-    
+
     assert(heapSize + size <= HEAP_CAPACITY);
 
     // starting of a free heap block
     void *ptr = heap + heapSize;
     heapSize += size;
-    
+
     chunkListInsert(&allocatedChunks, ptr, size);
     return ptr;
 }
 
-void heapFree (void *ptr)
+void heapFree(void *ptr)
 {
-    //find the index, move it to the freed chunks table, remove it from the allocated chunks table
-    const int index = chunkListFind(&allocatedChunks, ptr);
-    assert(index >= 0);
+    if (ptr != NULL)
+    {
+        // find the index, move it to the freed chunks table, remove it from the allocated chunks table
+        const int index = chunkListFind(&allocatedChunks, ptr);
+        assert(index >= 0);
 
-    chunkListInsert(&freedChunks, allocatedChunks.chunks[index].start, allocatedChunks.chunks[index].size);
-    chunkListRemove(&allocatedChunks, (size_t) index);
-
+        chunkListInsert(&freedChunks, allocatedChunks.chunks[index].start, allocatedChunks.chunks[index].size);
+        chunkListRemove(&allocatedChunks, (size_t)index);
+    }
 }
 
 // slide over the heap with a window of 8 bytes
 // and look for allocated memory resembling a pointer to a heap block
 // if we find a pointer that is unreachable, a pointer is reachable if it is pointing to another allocated block of memory on the heap
 // this hence becomes are automatic garbage collector
-void heapCollect ()
+void heapCollect()
 {
     // this should scan for pointer to the stack as well
     assert(false && "heapCollect: Not implemented");
 }
 
-int main (void)
+int main(void)
 {
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 10; i++)
     {
         void *p = heapAllocate(i);
-        if (i%2 == 0)
+        if (i % 2 == 0)
             heapFree(p);
     }
 
     chunkListDump(&allocatedChunks);
-    //heapFree(root1);
+    // heapFree(root1);
 
     return 0;
 }
---
+++ gitPush.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add .
+git commit -m "implementation of Separate Data Struc"
+git push -u origin main
